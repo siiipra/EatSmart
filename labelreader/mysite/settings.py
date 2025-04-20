7cc3f5b8a6699c533793f1dd167b5a9a131b28f4
siiipra <imsipra.235@gmail.com>
2025-04-21 00:27:33 +0530

    Allowed eatsmart.onrender.com

--- labelreader/mysite/settings.py
+++ labelreader/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(@3(fc=-_1d$z+&)9#3g_2(usitz9onoe1py5x3&yd0ne(xj+s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'eatsmart.onrender.com']
 
 
 # Application definition
